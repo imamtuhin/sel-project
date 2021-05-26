e570662c85da4ac276abe6b22510d696ca64bd86
imamtuhin <71822547+imamtuhin@users.noreply.github.com>
2021-05-26 20:04:14 +0600

    Add files via upload

---
+++ DATABASE/bookshop_pro.sql
@@ -0,0 +1,572 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 26, 2021 at 03:37 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookshop_pro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`user_id`, `firstname`, `lastname`, `email`, `username`, `password`) VALUES
+(5, 'imam', 'tuhin', 'imam@gmail.com', 'imam', 'a1Bz20ydqelm8m1wqleaccb8ea6090a40a98aa28c071810371');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(2, 'New'),
+(3, 'Old');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cust_id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `log_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action` varchar(100) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`log_id`, `user_id`, `action`, `date`) VALUES
+(77, 6, 'has logged in the system at ', '2017-11-11 00:26:10'),
+(78, 1, '(Administrator) has logged in the system at ', '2017-11-11 01:38:51'),
+(79, 6, 'has logged in the system at ', '2017-11-12 01:36:32'),
+(80, 6, 'has logged in the system at ', '2017-11-12 21:22:19'),
+(81, 1, '(Administrator) has logged in the system at ', '2017-11-12 21:25:48'),
+(82, 1, '(Administrator) has logged in the system at ', '2017-11-12 21:26:22'),
+(83, 2, '(Administrator) has logged in the system at ', '2017-11-12 21:29:04'),
+(84, 6, 'has logged in the system at ', '2017-11-12 21:45:12'),
+(85, 2, '(Administrator) has logged in the system at ', '2017-11-12 21:47:14'),
+(86, 6, 'has logged in the system at ', '2017-11-12 23:14:12'),
+(87, 1, '(Administrator) has logged in the system at ', '2017-11-12 23:19:55'),
+(88, 6, 'has logged in the system at ', '2017-11-12 23:22:32'),
+(89, 6, 'has logged in the system at ', '2017-11-13 00:17:25'),
+(90, 1, '(Administrator) has logged in the system at ', '2017-11-13 00:28:25'),
+(91, 1, 'added a new product 150 of Arduino Uno Rec3-1', '2017-11-13 00:31:30'),
+(92, 1, 'added a new product 400 of Aruino Mega', '2017-11-13 00:32:19'),
+(93, 1, 'added a new product 344 of Arduino Uno 2', '2017-11-13 00:33:17'),
+(94, 1, 'added a new product 234 of Raspberry Pi 3', '2017-11-13 00:34:22'),
+(95, 1, 'added a new product 456 of Flame Sensor', '2017-11-13 00:35:28'),
+(96, 6, 'has logged in the system at ', '2017-11-13 00:38:32'),
+(97, 1, '(Administrator) has logged in the system at ', '2017-11-13 08:45:06'),
+(98, 6, 'has logged in the system at ', '2017-11-13 08:47:34'),
+(99, 1, '(Administrator) has logged in the system at ', '2017-11-13 08:53:46'),
+(100, 7, 'has logged in the system at ', '2017-11-13 08:56:45'),
+(101, 1, '(Administrator) has logged in the system at ', '2017-11-13 10:40:50'),
+(102, 6, 'has logged in the system at ', '2017-11-13 10:42:37'),
+(103, 1, '(Administrator) has logged in the system at ', '2017-11-13 10:55:02'),
+(104, 6, 'has logged in the system at ', '2017-11-13 10:55:19'),
+(105, 1, '(Administrator) has logged in the system at ', '2017-11-13 11:15:27'),
+(106, 6, 'has logged in the system at ', '2017-11-13 11:15:38'),
+(107, 1, '(Administrator) has logged in the system at ', '2017-11-13 11:31:48'),
+(108, 6, 'has logged in the system at ', '2017-11-13 11:55:12'),
+(109, 1, '(Administrator) has logged in the system at ', '2017-11-13 11:57:27'),
+(110, 6, 'has logged in the system at ', '2017-11-13 11:59:22'),
+(111, 1, '(Administrator) has logged in the system at ', '2017-11-13 12:00:16'),
+(112, 6, 'has logged in the system at ', '2017-11-13 12:04:41'),
+(113, 8, 'has logged in the system at ', '2017-11-13 13:05:00'),
+(114, 2, '(Administrator) has logged in the system at ', '2017-11-13 13:16:17'),
+(115, 2, 'added a new product 700 of Sensor', '2017-11-13 13:20:38'),
+(116, 2, 'added 900 of Arduino Uno 2', '2017-11-13 13:20:57'),
+(117, 6, 'has logged in the system at ', '2017-11-13 19:58:52'),
+(118, 8, 'has logged in the system at ', '2017-11-13 20:00:59'),
+(119, 1, '(Administrator) has logged in the system at ', '2017-11-13 20:01:58'),
+(120, 1, '(Administrator) has logged in the system at ', '2017-11-13 21:47:41'),
+(121, 6, 'has logged in the system at ', '2017-11-13 21:49:55'),
+(122, 1, '(Administrator) has logged in the system at ', '2017-11-13 21:52:28'),
+(123, 1, '(Administrator) has logged in the system at ', '2017-11-14 16:01:08'),
+(124, 6, 'has logged in the system at ', '2017-11-17 01:43:42'),
+(125, 6, 'has logged in the system at ', '2017-11-17 02:15:46'),
+(126, 8, 'has logged in the system at ', '2017-11-21 20:19:39'),
+(127, 8, 'has logged in the system at ', '2017-11-25 23:31:53'),
+(128, 9, 'has logged in the system at ', '2018-10-12 19:52:39'),
+(129, 9, 'has logged in the system at ', '2018-10-13 01:18:49'),
+(130, 9, 'added a new product 26 of X9 THOR - Gaming Mouse', '2018-10-13 01:32:00'),
+(131, 9, 'has logged in the system at ', '2018-10-13 01:50:19'),
+(132, 10, 'has logged in the system at ', '2021-04-22 04:15:18'),
+(133, 11, 'has logged in the system at ', '2021-04-22 04:32:58'),
+(134, 11, 'has logged in the system at ', '2021-05-23 22:11:51'),
+(135, 11, 'has logged in the system at ', '2021-05-23 23:30:38'),
+(136, 11, 'has logged in the system at ', '2021-05-23 23:42:58'),
+(137, 11, 'has logged in the system at ', '2021-05-24 01:18:12'),
+(138, 11, 'added a new product 100 of Artificial intelligence', '2021-05-24 01:35:34'),
+(139, 11, 'has logged in the system at ', '2021-05-24 01:56:02'),
+(140, 11, 'has logged in the system at ', '2021-05-24 02:11:15'),
+(141, 11, 'has logged in the system at ', '2021-05-26 02:11:57'),
+(142, 11, 'has logged in the system at ', '2021-05-26 02:33:42'),
+(143, 5, '(Administrator) has logged in the system at ', '2021-05-26 02:38:04'),
+(144, 5, 'added a new product 100 of Artificial intelligence', '2021-05-26 02:46:13'),
+(145, 11, 'has logged in the system at ', '2021-05-26 02:57:03'),
+(146, 5, '(Administrator) has logged in the system at ', '2021-05-26 03:17:37'),
+(147, 5, 'added a new product 50 of software engineer', '2021-05-26 03:20:32'),
+(148, 5, 'added a new product 90 of software engineer', '2021-05-26 03:23:33'),
+(149, 11, 'has logged in the system at ', '2021-05-26 03:30:20'),
+(150, 11, 'has logged in the system at ', '2021-05-26 03:35:31'),
+(151, 11, 'has logged in the system at ', '2021-05-26 14:47:35'),
+(152, 11, 'has logged in the system at ', '2021-05-26 21:21:59'),
+(153, 5, '(Administrator) has logged in the system at ', '2021-05-26 21:24:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `track_num` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact` varchar(50) NOT NULL,
+  `shipping_add` varchar(500) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `totalprice` decimal(10,2) NOT NULL,
+  `tax` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_details_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `prod_qty` int(11) NOT NULL,
+  `total_qty` varchar(30) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_id` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`order_details_id`, `prod_id`, `prod_qty`, `total_qty`, `total`, `user_id`, `order_id`) VALUES
+(104, 14, 1, '221', '760.00', 9, '1'),
+(105, 17, 1, '25', '2200.00', 9, '1'),
+(106, 12, 3, '388', '465.00', 10, '1'),
+(108, 12, 5, '383', '775.00', 11, '1'),
+(109, 16, 3, '696', '4500.00', 11, '1'),
+(110, 14, 1, '220', '760.00', 11, '1'),
+(111, 14, 1, '219', '760.00', 11, '1'),
+(112, 18, 5, '95', '750.00', 11, '1'),
+(113, 16, 4, '696', '6000.00', 11, '1'),
+(114, 12, 6, '377', '930.00', 11, '1'),
+(115, 18, 7, '88', '1050.00', 11, '1'),
+(116, 18, 5, '83', '750.00', 11, '1'),
+(117, 19, 1, '99', '150.00', 11, '1'),
+(118, 20, 1, '49', '140.00', 11, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL,
+  `sales_id` int(11) NOT NULL,
+  `payment` decimal(10,2) NOT NULL,
+  `payment_date` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `due` decimal(10,2) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `or_no` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `prod_id` int(11) NOT NULL,
+  `prod_name` varchar(50) NOT NULL,
+  `prod_desc` varchar(500) NOT NULL,
+  `prod_qty` int(11) NOT NULL,
+  `prod_cost` decimal(10,2) NOT NULL,
+  `prod_price` decimal(10,2) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `supplier` varchar(100) NOT NULL,
+  `prod_serial` varchar(50) NOT NULL,
+  `prod_pic1` varchar(500) NOT NULL,
+  `prod_pic2` varchar(500) NOT NULL,
+  `prod_pic3` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`prod_id`, `prod_name`, `prod_desc`, `prod_qty`, `prod_cost`, `prod_price`, `category`, `supplier`, `prod_serial`, `prod_pic1`, `prod_pic2`, `prod_pic3`) VALUES
+(19, 'Artificial intelligence', 'A Modern Approach · Fourth edition, 2020 · by Stuart Russell and Peter Norvig.', 99, '140.00', '150.00', 'New', 'daraz', '123-01', 'ai.jpg', 'ai2.jpg', 'ai3.jpg'),
+(20, 'software engineer', 'A Book For Beginners', 49, '135.00', '140.00', 'New', 'BoiBazar', '123-02', 'see3.png', 'see2.jpg', 'see1.jpg'),
+(21, 'software engineer', 'A Book For engineer', 90, '190.00', '200.00', 'New', 'rokomari', '123-03', 'se2.jpg', 'se1.jpg', 'se3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `sales_id` int(11) NOT NULL,
+  `cust_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount_due` decimal(10,2) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `mode_of_payment` varchar(100) NOT NULL,
+  `total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_details`
+--
+
+CREATE TABLE `sales_details` (
+  `sales_details_id` int(11) NOT NULL,
+  `sales_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supp_id` int(11) NOT NULL,
+  `supp_name` varchar(100) NOT NULL,
+  `supp_address` varchar(200) NOT NULL,
+  `supp_contact` varchar(50) NOT NULL,
+  `supp_email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`supp_id`, `supp_name`, `supp_address`, `supp_contact`, `supp_email`) VALUES
+(1, 'BoiBazar', 'dhaka', '987654321', 'boibazar!gmail.com'),
+(2, 'daraz', 'tejgaon', '12345678910', 'daraz@gmail.com'),
+(4, 'rokomari', 'dhaka', '010000000', 'rakomari@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_trans`
+--
+
+CREATE TABLE `temp_trans` (
+  `temp_trans_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `trans_id` int(11) NOT NULL,
+  `or_no` int(11) NOT NULL,
+  `prod_serial` varchar(50) NOT NULL,
+  `prod_name` varchar(100) NOT NULL,
+  `trans_qty` int(11) NOT NULL,
+  `ppi` decimal(10,0) NOT NULL,
+  `cust_fullname` varchar(100) NOT NULL,
+  `transdate` datetime NOT NULL,
+  `tax` decimal(10,2) NOT NULL,
+  `total` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `firstname`, `middlename`, `lastname`, `address`, `email`, `contact`, `username`, `password`) VALUES
+(11, 'imam', '.', 'tuhin', 'uttara', 'imam@gmail.com', '01000000000', 'imamtuhin', 'a1Bz20ydqelm8m1wql856ed86af440e18b8813b16ac6b7a199');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cust_id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`log_id`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`order_details_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sales_id`);
+
+--
+-- Indexes for table `sales_details`
+--
+ALTER TABLE `sales_details`
+  ADD PRIMARY KEY (`sales_details_id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supp_id`);
+
+--
+-- Indexes for table `temp_trans`
+--
+ALTER TABLE `temp_trans`
+  ADD PRIMARY KEY (`temp_trans_id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`trans_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=154;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `order_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sales_details`
+--
+ALTER TABLE `sales_details`
+  MODIFY `sales_details_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `supp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `temp_trans`
+--
+ALTER TABLE `temp_trans`
+  MODIFY `temp_trans_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `trans_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
